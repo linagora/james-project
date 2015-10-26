02a30bfa0ca8bafa30b55cf0fa9e9464e87be561
Matthieu Baechler <matthieu.baechler@gmail.com>
2015-10-26 15:21:51 +0100

    fixup! JAMES-1626 - bootstrap guice-based application with Cassandra IMAP server

--- server/container/cassandra-guice/src/main/java/org/apache/james/modules/mailbox/CassandraMailboxModule.java
+++ server/container/cassandra-guice/src/main/java/org/apache/james/modules/mailbox/CassandraMailboxModule.java
@@ -18,25 +18,32 @@
  ****************************************************************/
 package org.apache.james.modules.mailbox;
 
-import com.google.inject.name.Names;
+import javax.inject.Singleton;
+
 import org.apache.james.adapter.mailbox.store.UserRepositoryAuthenticator;
 import org.apache.james.mailbox.MailboxManager;
 import org.apache.james.mailbox.MailboxPathLocker;
 import org.apache.james.mailbox.SubscriptionManager;
-import org.apache.james.mailbox.cassandra.*;
+import org.apache.james.mailbox.cassandra.CassandraId;
+import org.apache.james.mailbox.cassandra.CassandraMailboxManager;
+import org.apache.james.mailbox.cassandra.CassandraMailboxSessionMapperFactory;
+import org.apache.james.mailbox.cassandra.CassandraSubscriptionManager;
 import org.apache.james.mailbox.cassandra.mail.CassandraModSeqProvider;
 import org.apache.james.mailbox.cassandra.mail.CassandraUidProvider;
 import org.apache.james.mailbox.elasticsearch.events.ElasticSearchListeningMessageSearchIndex;
+import org.apache.james.mailbox.exception.MailboxException;
 import org.apache.james.mailbox.store.Authenticator;
 import org.apache.james.mailbox.store.MailboxSessionMapperFactory;
 import org.apache.james.mailbox.store.NoMailboxPathLocker;
 import org.apache.james.mailbox.store.mail.MessageMapperFactory;
 import org.apache.james.mailbox.store.mail.ModSeqProvider;
 import org.apache.james.mailbox.store.mail.UidProvider;
+import org.apache.james.mailbox.store.search.MessageSearchIndex;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
 import com.google.inject.TypeLiteral;
-import org.apache.james.mailbox.store.search.MessageSearchIndex;
+import com.google.inject.name.Named;
 
 public class CassandraMailboxModule extends AbstractModule {
 
@@ -44,8 +51,6 @@ public class CassandraMailboxModule extends AbstractModule {
 
     @Override
     protected void configure() {
-        bind(MailboxManager.class).annotatedWith(Names.named(MAILBOXMANAGER_NAME)).to(CassandraMailboxManager.class);
-
         bind(new TypeLiteral<MessageSearchIndex<CassandraId>>(){}).to(new TypeLiteral<ElasticSearchListeningMessageSearchIndex<CassandraId>>(){});
 
         bind(SubscriptionManager.class).to(CassandraSubscriptionManager.class);
@@ -57,5 +62,11 @@ public class CassandraMailboxModule extends AbstractModule {
 
         bind(new TypeLiteral<ModSeqProvider<CassandraId>>(){}).to(new TypeLiteral<CassandraModSeqProvider>(){});
         bind(new TypeLiteral<UidProvider<CassandraId>>(){}).to(new TypeLiteral<CassandraUidProvider>(){});
+    }
+    
+    @Provides @Named(MAILBOXMANAGER_NAME) @Singleton
+    public MailboxManager provideMailboxManager(CassandraMailboxManager cassandraMailboxManager) throws MailboxException {
+        cassandraMailboxManager.init();
+        return cassandraMailboxManager;
     }
 }
