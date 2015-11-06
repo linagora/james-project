46be4a40564c1df34cffda1e983e21d1beef773f
Raphael Ouazana <raphael.ouazana@linagora.com>
2015-11-06 14:20:04 +0100

    ISSUE-41: getMailboxes should return error on unimplemented fields

--- server/protocols/jmap/src/main/java/org/apache/james/jmap/model/Role.java
+++ server/protocols/jmap/src/main/java/org/apache/james/jmap/model/Role.java
@@ -16,28 +16,35 @@
  * specific language governing permissions and limitations      *
  * under the License.                                           *
  ****************************************************************/
+package org.apache.james.jmap.model;
 
-package org.apache.james.jmap;
+public enum Role {
 
-import org.apache.james.jmap.methods.GetMailboxesMethod;
-import org.apache.james.jmap.methods.JmapRequestParser;
-import org.apache.james.jmap.methods.JmapRequestParserImpl;
-import org.apache.james.jmap.methods.JmapResponseWriter;
-import org.apache.james.jmap.methods.JmapResponseWriterImpl;
-import org.apache.james.jmap.methods.Method;
+    INBOX("inbox"),
+    ARCHIVE("archive"),
+    DRAFTS("drafts"),
+    OUTBOX("outbox"),
+    SENT("sent"),
+    TRASH("trash"),
+    SPAM("spam"),
+    TEMPLATES("templates");
+    
+    private String name;
 
-import com.google.inject.AbstractModule;
-import com.google.inject.multibindings.Multibinder;
-
-public class MethodsModule extends AbstractModule {
-
-    @Override
-    protected void configure() {
-        bind(JmapRequestParser.class).to(JmapRequestParserImpl.class).asEagerSingleton();
-        bind(JmapResponseWriter.class).to(JmapResponseWriterImpl.class).asEagerSingleton();
-
-        Multibinder<Method> methods = Multibinder.newSetBinder(binder(), Method.class);
-        methods.addBinding().to(GetMailboxesMethod.class);
+    private Role(String name) {
+        this.name = name;
     }
 
+    public static Role from(String name) {
+        for (Role role : values()) {
+            if (role.serialize().equals(name.toLowerCase())) {
+                return role;
+            }
+        }
+        return null;
+    }
+
+    public String serialize() {
+        return name;
+    }
 }
