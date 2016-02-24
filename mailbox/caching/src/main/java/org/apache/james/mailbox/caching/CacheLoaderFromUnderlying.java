32c03aca0b8ac23470c73d28753dab96962090ff
Antoine Duprat <aduprat@linagora.com>
2016-02-24 08:53:17 +0100

    WIP only test container

--- mailbox/caching/src/main/java/org/apache/james/mailbox/caching/CacheLoaderFromUnderlying.java
+++
@@ -1,5 +0,0 @@
-package org.apache.james.mailbox.caching;
-
-public interface CacheLoaderFromUnderlying<Key, Value, Underlying, Except extends Throwable> {
-	Value load(Key key, Underlying underlying) throws Except;
-}
--- mpt/core/src/main/java/org/apache/james/mpt/api/Continuation.java
+++
@@ -1,7 +0,0 @@
-package org.apache.james.mpt.api;
-
-public interface Continuation {
-
-    void doContinue();
-
-}
