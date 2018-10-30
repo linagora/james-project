7d90d945ba6962d4dcca818d5c30bd69a54240f2
kratostaine <madhu.bhat.8j@gmail.com>
2018-10-30 20:32:57 +0530

    JAMES-2574 Removed tabs and added Apache v2 header

--- server/protocols/webadmin/webadmin-core/src/main/java/org/apache/james/webadmin/routes/HealthCheckRoutes.java
+++ server/protocols/webadmin/webadmin-core/src/main/java/org/apache/james/webadmin/routes/HealthCheckRoutes.java
@@ -53,7 +53,7 @@ public class HealthCheckRoutes implements PublicRoutes {
 
     public static final String HEALTHCHECK = "/healthcheck";
     public static final String CHECKS = "/checks";
-    
+
     private final Set<HealthCheck> healthChecks;
     private final JsonTransformer jsonTransformer;
 
@@ -71,7 +71,7 @@ public class HealthCheckRoutes implements PublicRoutes {
     @Override
     public void define(Service service) {
         service.get(HEALTHCHECK, this::validateHealthchecks, jsonTransformer);
-        service.get(HEALTHCHECK + CHECKS, this::getHealthChecks, jsonTransformer);
+        service.get(HEALTHCHECK + CHECKS, this::getHealthchecks, jsonTransformer);
     }
 
     @GET
@@ -87,19 +87,15 @@ public class HealthCheckRoutes implements PublicRoutes {
 		response.status(getCorrespondingStatusCode(anyUnhealthyOrDegraded));
 		return response;
 	}
-    
+
     @GET
     @Path(CHECKS)
     @ApiOperation(value = "List all health checks")
-    @ApiResponses(value = {
-            @ApiResponse(code = HttpStatus.OK_200, message = "List of all health checks", 
-            		response = HealthCheckDto.class, responseContainer = "List"),
-            @ApiResponse(code = HttpStatus.NOT_FOUND_404, message = "No health checks found")
-    })
-    public Object getHealthChecks(Request request, Response response) {
-		List<HealthCheckDto> checks = healthChecks.stream()
-				.map(healthCheck -> new HealthCheckDto(healthCheck.componentName())).collect(Guavate.toImmutableList());
-		return checks;
+    @ApiResponse(code = HttpStatus.OK_200, message = "List of all health checks",
+                response = HealthCheckDto.class, responseContainer = "List")
+    public Object getHealthchecks(Request request, Response response) {
+        return healthChecks.stream()
+            .map(healthCheck -> new HealthCheckDto(healthCheck.componentName())).collect(Guavate.toImmutableList());
     }
 
     private int getCorrespondingStatusCode(List<Result> anyUnhealthy) {
