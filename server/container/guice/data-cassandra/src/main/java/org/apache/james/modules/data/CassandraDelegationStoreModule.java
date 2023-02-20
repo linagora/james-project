b6a5c0b720bb028e6bb6f9a31ccc6d41dbd75d40
Benoit Tellier <btellier@linagora.com>
2023-02-20 16:37:01 +0700

    JAMES-3885 Delegation should be supported when using LDAP

--- server/container/guice/data-cassandra/src/main/java/org/apache/james/modules/data/CassandraDelegationStoreModule.java
+++ server/container/guice/data-cassandra/src/main/java/org/apache/james/modules/data/CassandraDelegationStoreModule.java
@@ -16,29 +16,29 @@
  * specific language governing permissions and limitations      *
  * under the License.                                           *
  ****************************************************************/
+
 package org.apache.james.modules.data;
 
-import org.apache.james.server.core.configuration.ConfigurationProvider;
-import org.apache.james.user.api.UsersRepository;
-import org.apache.james.user.jpa.JPAUsersRepository;
-import org.apache.james.utils.InitializationOperation;
-import org.apache.james.utils.InitilizationOperationBuilder;
+import org.apache.james.backends.cassandra.components.CassandraModule;
+import org.apache.james.user.api.DelegationStore;
+import org.apache.james.user.api.DelegationUsernameChangeTaskStep;
+import org.apache.james.user.api.UsernameChangeTaskStep;
+import org.apache.james.user.cassandra.CassandraDelegationStore;
+import org.apache.james.user.cassandra.CassandraUsersDAO;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
-import com.google.inject.multibindings.ProvidesIntoSet;
+import com.google.inject.multibindings.Multibinder;
 
-public class JPAUsersRepositoryModule extends AbstractModule {
+public class CassandraDelegationStoreModule extends AbstractModule {
     @Override
     public void configure() {
-        bind(JPAUsersRepository.class).in(Scopes.SINGLETON);
-        bind(UsersRepository.class).to(JPAUsersRepository.class);
-    }
+        bind(DelegationStore.class).to(CassandraDelegationStore.class);
+        Multibinder<CassandraModule> cassandraDataDefinitions = Multibinder.newSetBinder(binder(), CassandraModule.class);
+        cassandraDataDefinitions.addBinding().toInstance(org.apache.james.user.cassandra.CassandraUsersRepositoryModule.MODULE);
 
-    @ProvidesIntoSet
-    InitializationOperation configureJpaUsers(ConfigurationProvider configurationProvider, JPAUsersRepository usersRepository) {
-        return InitilizationOperationBuilder
-            .forClass(JPAUsersRepository.class)
-            .init(() -> usersRepository.configure(configurationProvider.getConfiguration("usersrepository")));
+        Multibinder.newSetBinder(binder(), UsernameChangeTaskStep.class)
+            .addBinding().to(DelegationUsernameChangeTaskStep.class);
+        bind(CassandraUsersDAO.class).in(Scopes.SINGLETON);
     }
 }
